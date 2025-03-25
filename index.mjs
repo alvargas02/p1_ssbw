2508102aa31ed621327cf685806c56cde460bb62
Alejandro <alejandrolvargas2002@gmail.com>
2025-03-25 17:13:47 +0100

    feat: :sparkles: Task 1
    Landing Page with Bulma template

--- index.mjs
+++ index.mjs
@@ -1,32 +1,27 @@
-import express   from 'express'
-import nunjucks  from 'nunjucks'
+import express  from 'express';
+import nunjucks from 'nunjucks';
 
-const IN = process.env.IN || 'development'
-const app = express()
+const IN = process.env.IN || 'development';
+const app = express();
 
-// Configurar Nunjucks para utilizar la carpeta 'views'
+// Configurar Nunjucks para usar la carpeta 'views'
 nunjucks.configure('views', {
-	autoescape: true,
-	noCache:    IN === 'development',
-	watch:      IN === 'development',
-	express: app
-})
-app.set('view engine', 'html')
+  autoescape: true,
+  noCache:    IN === 'development',
+  watch:      IN === 'development',
+  express: app
+});
+app.set('view engine', 'html');
 
-// Middleware para servir archivos estáticos desde la carpeta 'public'
-app.use(express.static('public'))
+// Middleware para servir archivos estáticos (CSS, JS, imágenes, etc.)
+app.use(express.static('public'));
 
-// Ruta de prueba para mostrar que el servidor funciona
-app.get('/hola', (req, res) => {
-	res.send('Hola desde el servidor');
-})
-
-// Ruta para la Landing Page, renderizando la plantilla index.njk
+// Ruta para la landing page
 app.get('/', (req, res) => {
-	res.render("index.njk", { saludo: 'Bienvenido al Museo Arqueológico de Granada' })
-})
+  res.render("index.njk");
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-	console.log(`Servidor ejecutándose en http://localhost:${PORT} en modo ${IN}`);
-})
+  console.log(`Servidor ejecutándose en http://localhost:${PORT} en modo ${IN}`);
+});
